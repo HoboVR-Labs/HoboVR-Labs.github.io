cd896a70c36cbdc687102158b17114a7b1066f24
ix5 <ix5@users.noreply.github.com>
2022-05-27 06:42:40 +0200

    main.py: Simplify script routes

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
+from os import path as os_path
+
+import werkzeug
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def show_root():
-    return send_from_directory("", "index.html")
-
-
-@app.route("/<path>")
-def show(path):
-    return send_from_directory("", path)
-
-
-# @app.route("/contact/")
-# def show_contacts():
-#     return send_from_directory("", "contact.html")
-
-
-# @app.route("/team/")
-# def show_team():
-#     return send_from_directory("", "team.html")
-
-
-@app.route("/style.css")
-def show_style():
-    return send_from_directory("", "style.css")
-
-
-@app.route("/img/<path>")
-def show_img(path):
-    return send_from_directory("img", path)
-
-
-@app.route("/docs/html/")
-def show_docs():
-    return send_from_directory("./docs/html/", "index.html")
-
-
-@app.route("/docs/html/<path>")
-def show_docs2(path):
-    return send_from_directory("./docs/html/", path)
-
-
-# @app.route("/favicon.png")
-# def show_favicon():
-#     return send_from_directory("img", "favicon.png")
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>')
+def staticHost(path):
+    print("path: %s" % path)
+    normalized = path.strip("/")
+    if os_path.isfile(normalized + ".html"):
+        return send_from_directory("", normalized + ".html")
+    try:
+        return send_from_directory("", path)
+    except werkzeug.exceptions.NotFound as e:
+        if os_path.isfile(normalized + "/index.html"):
+            return send_from_directory("", normalized + "/index.html")
+        raise e
